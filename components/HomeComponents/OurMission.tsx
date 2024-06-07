5b750cb2799532d4707444b504ad47d749d33792
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-06-07 19:56:30 +0530

    change the css

--- components/HomeComponents/OurMission.tsx
+++ components/HomeComponents/OurMission.tsx
@@ -6,21 +6,21 @@ const OurMission = () => {
       <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2">
         <div></div>
         <div className="flex flex-col items-center justify-center w-full min-h-screen gap-5">
-          <div className="bg-white dark:bg-black dark:text-white text-black w-[60%] p-12 rounded-3xl ">
+          <div className="bg-white dark:bg-black dark:text-white text-black lg:w-[60%] p-12 rounded-3xl ">
             <h5 className="text-xl font-bold">Our Mission</h5>
             <p>
               The most well-known dummy text is the Lorem Ipsum, which is said
               to have originated in the 16th century.
             </p>
           </div>
-          <div className="bg-white dark:bg-black dark:text-white text-black w-[60%] p-12 rounded-3xl ">
+          <div className="bg-white dark:bg-black dark:text-white text-black lg:w-[60%] p-12 rounded-3xl ">
             <h5 className="text-xl font-bold">Our Vision</h5>
             <p>
               The most well-known dummy text is the Lorem Ipsum, which is said
               to have originated in the 16th century.
             </p>
           </div>
-          <div className="bg-white dark:bg-black dark:text-white text-black w-[60%] p-12 rounded-3xl ">
+          <div className="bg-white dark:bg-black dark:text-white text-black lg:w-[60%] p-12 rounded-3xl ">
             <h5 className="text-xl font-bold">Who We Are ?</h5>
             <p>
               The most well-known dummy text is the Lorem Ipsum, which is said
